1609bf83583348c79876a4deab9463eaa58a911e
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-02-23 13:43:36 +0100

    mijn oefening 1 en 2

---
+++ javascript/oefening_1/oefening_1.js
@@ -0,0 +1,17 @@
+
+//invoer
+let getal_1 = parseInt(prompt("Getal 1"));
+let getal_2 = parseInt(prompt("Getal 2"));
+
+// verwerking
+
+let optelling = getal_1+getal_2;
+let aftrekking = getal_1 - getal_2 ;
+let vermenigvuldiging = getal_1 * getal_2 ;
+let deling = getal_1 / getal_2 ;
+
+// uitvoer
+console.log("Optelling van de twee getallen: " + optelling);
+console.log("Aftrekking van de twee getallen: " + aftrekking);
+console.log("Vermenigvuldiging van de twee getallen: " + vermenigvuldiging);
+console.log("Deling van de twee getallen: " + deling);
---
+++ javascript/variabelen/oefening_2/oefening_2.js
@@ -0,0 +1,13 @@
+//invoer
+
+let graad = parseInt(prompt("Hoevel graden?"))
+
+
+//verwerking
+let celNaarFahrenheit = ((graad * 9/5) + 32);
+
+
+//uitvoer
+
+console.log(graad + "°C = " + celNaarFahrenheit + "°F");
+
