654d5daffe42feea70da9503163cdc99bc0790bd
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-02-24 16:16:53 +0100

    oefening 3

--- javascript/voorwaardes/oefening_3/oefening_3.js
+++ javascript/voorwaardes/oefening_3/oefening_3.js
@@ -9,8 +9,11 @@ let naam = delen_at[0];
 let domein = delen_at[1];
 let toplevel_domein = delen_punt[1];
 
+let plaats_at = e_mail.indexOf("@");
+let plaats_punt = e_mail.indexOf(".");
+
 //uitvoer
-if (e_mail.includes("@") && e_mail.includes(".")) {
+if (plaats_at>1 && plaats_at<plaats_punt+2 && e_mail.length > plaats_punt+1 ) {
     console.log("e-mail: " + e_mail);
     console.log("naam: " + naam);
     console.log("domein: " + domein);
