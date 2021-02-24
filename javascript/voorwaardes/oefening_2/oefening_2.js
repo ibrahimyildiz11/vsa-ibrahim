a69c3c8310ff7b7d8270516a5300ae854e4e6851
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-02-24 14:39:08 +0100

    oefening 2 veranderd op basis feedback

--- javascript/voorwaardes/oefening_2/oefening_2.js
+++ javascript/voorwaardes/oefening_2/oefening_2.js
@@ -1,68 +1,38 @@
 //invoer
 let getal = parseInt(prompt("Geef een getal: "));
 
-let resultaat = "";
+
 
 //verwerking
 
-/*if (getal<1) {
-    bericht = "Getal moet groter zijn of gelijk aan 1";
-}
-
-else if (getal >= 1 && getal <= 10) {
-    bericht = "Het is een getal tussen 1 en 10";
-}
-else if (getal > 10 && getal <= 20) {
-    bericht = "Het is een getal tussen 11 en 20";
-}
-else if (getal > 20 && getal <= 30) {
-    bericht = "Het is een getal tussen 21 en 30";
-}
-
-else if (getal >30) {
-    bericht = "Het is een getal boven 30";
-} */
 
 // feedback: deze is goed, ik zou hier wel meteen de variabele "berichtje" een waarde geven
+let berichtje = "";
+
 if (getal<1) {
-    resultaat = "situatie_1";
+    berichtje = "Getal moet groter zijn of gelijk aan 1";
 }
 // feedback: in binnen het "else" onderdeel kan de switch komen om de voorwaardes te controleren
-
-else if (getal >= 1 && getal <= 10) {
-    resultaat = "situatie_2";
-}
-else if (getal > 10 && getal <= 20) {
-    resultaat = "situatie_3";
-}
-else if (getal > 20 && getal <= 30) {
-    resultaat = "situatie_4";
-}
-
-else if (getal >30) {
-    resultaat = "situatie_5";
-} 
-
 // feedback: probeer switch (true) { }
-switch (resultaat) {
-    case 'situatie_1':
-        berichtje = "Getal moet groter zijn of gelijk aan 1";
-        break;
 
-    case 'situatie_2':
-        berichtje = "Het is een getal tussen 1 en 10";
-        break;
-
-    case 'situatie_3':
-        berichtje = "Het is een getal tussen 11 en 20";
-        break;
-
-    case 'situatie_4':
-        berichtje = "Het is een getal tussen 21 en 30";
-        break;
-
-    default:
-        berichtje = "Het is een getal boven 30";
+else {
+    switch (true) {
+        case (getal >= 1 && getal <= 10):
+            berichtje = "Het is een getal tussen 1 en 10";
+            break;
+        
+        case (getal > 10 && getal <= 20):
+            berichtje = "Het is een getal tussen 11 en 20";
+            break;
+        
+        case (getal > 20 && getal <= 30):
+            berichtje = "Het is een getal tussen 21 en 30";
+            break;
+    
+        default:
+            berichtje = "Het is een getal boven 30"; 
+            break;
+    }
 }
 
 
