afe3f389cc2cea0e4420182ac4600ed262b4d2e7
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-02-24 11:45:55 +0100

    voorwaardes oefening 1

---
+++ javascript/voorwaardes/oefening_1/oefening_1.js
@@ -0,0 +1,23 @@
+//invoer
+let getallen = prompt("Geef twee getallen gescheiden door een ','");
+
+//verwerking
+nummers = getallen.split(",");
+
+let getal_1 = nummers[0];
+let getal_2 = nummers[1];
+
+let situatie = "";
+
+if((getal_1 < 10 || getal_1 > 20) || (getal_2 < 10 || getal_2 > 20 )) {
+    situatie = "getal 1 of getal 2 niet tussen 10 en 20"
+}
+
+else {
+    situatie = "getal 1 en getal 2 tussen 10 en 20"
+}
+//uitvoer
+
+console.log("getal 1: " + getal_1);
+console.log("getal 2: " + getal_2);
+console.log(situatie);
