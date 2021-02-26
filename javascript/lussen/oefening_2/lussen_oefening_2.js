b0bb0b049ae48426fd6b8c7ca996fe8ccae9ee70
dataweb <bert@data-web.be>
2021-02-26 13:33:44 +0100

    Feedback lussen oefening 2

--- javascript/lussen/oefening_2/lussen_oefening_2.js
+++ javascript/lussen/oefening_2/lussen_oefening_2.js
@@ -1,6 +1,8 @@
+// feedback: "fibonacci"? "aantal_getallen" lijkt mee beter
 let fibonacci = parseInt(prompt("Hoeveel Fibonacci getallen tonen?"));
 
 let lijst = [0,1];
+// feedback: "x" vervangen door een nuttigere naam
 let x = 2;
 while (x < fibonacci) {
     lijst.push(lijst[lijst.length-1] + lijst[lijst.length-2] );
