3df37fe91fd9d1d59414d2a27b4f9bdad4093ffd
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-02 11:01:47 +0100

    Ik heb mijn dubbele negatie code verander

--- javascript/lussen/oefening_3/lussen_oefening_3.js
+++ javascript/lussen/oefening_3/lussen_oefening_3.js
@@ -5,13 +5,13 @@ let aantal_te_tonen = 30;
 
 for(let volgend_getal = 2; aantal_te_tonen>0; volgend_getal++){
     // feedback: dubbele negatie vermijden: niet false = true
-    let niet_priemgetal = false;
+    let priemgetal = true;
     for(let deler=2; deler<volgend_getal; deler++) {
         if(volgend_getal%deler===0) {
-            niet_priemgetal=true;
+            priemgetal=false;
         }
     }
-    if(niet_priemgetal===false){
+    if(priemgetal===true){
         priemgetal_lijst.push(volgend_getal);
         aantal_te_tonen--;
     }
