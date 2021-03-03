5870b0bd8b34f8dffcc0b60f09a8ee2cf4226125
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 15:51:07 +0100

    functie extra oefening 1 opnieuw

--- javascript/functies/extra_oefening_1/extra_oefening_1_functie.js
+++ javascript/functies/extra_oefening_1/extra_oefening_1_functie.js
@@ -1,17 +1,17 @@
 //invoer & verwerking
 function zinMetEersteLetterUppercase(zin){
     let zin_delen = zin.split(" ");
-    let uitvoer = "";
+    let eerste_letter_groot = "";
     zin_delen.forEach(function(woord){
-        uitvoer+= woord[0].toUpperCase() +  woord.substring(1)  + " "
+        eerste_letter_groot+= woord[0].toUpperCase() +  woord.substring(1)  + " "
     });
     // feedback: "uitvoer" is wat algemeen.
-    return uitvoer;
+    return eerste_letter_groot;
 }
 
 // feedback: voor nu is een_zin goed maar kan misschien iets specifieker
-let een_zin = zinMetEersteLetterUppercase(prompt("Geef een zin: "));
+let een_zin_groot_eerste_letter = zinMetEersteLetterUppercase(prompt("Geef een zin: "));
 
 //uitvoer
-console.log(een_zin)
+console.log(een_zin_groot_eerste_letter)
 
