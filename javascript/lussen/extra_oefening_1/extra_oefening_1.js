c7bd184c0eb9fff3165dca055272ca58abbc21ef
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 09:46:23 +0100

    extra lussen oefening1

---
+++ javascript/lussen/extra_oefening_1/extra_oefening_1.js
@@ -0,0 +1,15 @@
+
+//invoer
+let zin = prompt("Geef een zin: ");
+let zin_delen = zin.split(" ");
+let uitvoer = "";
+
+
+//verwerking
+zin_delen.forEach(function(woord){
+    uitvoer+= woord[0].toUpperCase() +  woord.substring(1)  + " "
+});
+
+//uitvoer
+console.log(uitvoer)
+
