792468ba911f954bb10faed98924c6ca2f6d1103
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 15:08:58 +0100

    Functie Oefening5

---
+++ javascript/functies/oefening_5/lussen_oefening_5_functie.js
@@ -0,0 +1,26 @@
+// invoer & verwerking
+
+function tabelMetGetallen(getallen_tabel){
+    let koppelteken = "-";
+    let uitvoer = "";
+    getallen_tabel.forEach(function(lijn){
+        uitvoer+= "|" + lijn.join("|") + "|" + "\n";
+    })
+    
+    let aantal_streepjes = uitvoer.length/getallen_tabel.length-getallen_tabel.length;
+    let streep = koppelteken.repeat(aantal_streepjes);
+    let resultaat = ("┌" + streep + "┐") + "\n" + uitvoer + ("└" + streep + "┘");
+
+    return resultaat;
+}
+
+let een_getallen_tabel = [    
+    [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10"],
+    ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+    ["21", "22", "23", "24", "25", "26", "27", "28", "29", "30"]
+];
+
+let een_tabel = tabelMetGetallen(een_getallen_tabel);
+
+// uitvoer
+console.log(een_tabel);
