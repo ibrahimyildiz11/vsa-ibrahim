f107a077aaa748cf6749f3d014447e5e2120b36e
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 10:20:22 +0100

    Oefening 5 het werkt en nu ook leesbare

--- javascript/lussen/oefening_5/lussen_oefening_5.js
+++ javascript/lussen/oefening_5/lussen_oefening_5.js
@@ -7,27 +7,16 @@ let getallen_tabel = [
 
 let koppelteken = "-";
 let uitvoer = "";
-// feedback: ook hier graag een for(each) lus gebruiken
-/*let lijn_een = getallen_tabel[0];
-let lijn_twee = getallen_tabel[1];
-let lijn_drie = getallen_tabel[2];*/
 
 // verwerking
-
 getallen_tabel.forEach(function(lijn){
     uitvoer+= "|" + lijn.join("|") + "|" + "\n";
 })
 
-// uitvoer
-// feedack: het is dynamisch maar nog steeds is 3 niet dynamisch... wat met meer rijen?
-console.log("┌" + koppelteken.repeat(uitvoer.length/getallen_tabel.length-getallen_tabel.length) + "┐");
-console.log(uitvoer);
-console.log("└" + koppelteken.repeat(uitvoer.length/getallen_tabel.length-getallen_tabel.length) + "┘");
+let aantal_streepjes = uitvoer.length/getallen_tabel.length-getallen_tabel.length;
+let streep = koppelteken.repeat(aantal_streepjes);
 
-// feedback: repeat() is goed, maar probeer 29 dynamisch te bepalen
-/*console.log("┌" + koppelteken.repeat(29) + "┐");
-console.log("|" + lijn_een.join("|")+ "|");
-console.log("|" + lijn_twee.join("|")+ "|");
-console.log("|" + lijn_drie.join("|")+ "|");
-// feedback: repeat() is goed, maar probeer 29 dynamisch te bepalen
-console.log("└" + koppelteken.repeat(29) + "┘");*/
+// uitvoer
+console.log("┌" + streep + "┐");
+console.log(uitvoer);
+console.log("└" + streep + "┘");
