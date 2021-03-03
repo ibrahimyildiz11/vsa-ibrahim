eb06cfba9b8bf711a88b9fb11645883de12c3c81
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 14:12:24 +0100

    Functie oefening3

---
+++ javascript/functies/oefening_3/lussen_oefening_3_functie.js
@@ -0,0 +1,20 @@
+
+function priemgetalTonen(aantal_te_tonen) {
+    let priemgetal_lijst = [];
+    for(let volgend_getal = 2; aantal_te_tonen>0; volgend_getal++){
+        let priemgetal = true;
+        for(let deler=2; deler<volgend_getal; deler++) {
+            if(volgend_getal%deler===0) {
+                priemgetal=false;
+            }
+        }
+        if(priemgetal===true){
+            priemgetal_lijst.push(volgend_getal);
+            aantal_te_tonen--;
+        }
+    }
+    return priemgetal_lijst;
+}
+
+let resultaat = priemgetalTonen(50);
+console.log(resultaat);
