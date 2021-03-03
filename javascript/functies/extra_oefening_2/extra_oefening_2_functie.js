824ed6eab3a16a541f583c3b9c121efb7e61036f
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 16:00:55 +0100

    functie extra oefening 2

---
+++ javascript/functies/extra_oefening_2/extra_oefening_2_functie.js
@@ -0,0 +1,21 @@
+
+function paswoordMaker(tekens){
+    if(tekens<4){
+        tekens= parseInt(prompt("Geef tekens groter dan 4:"));
+    }
+    
+    let keuze = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?%-_&";
+    let paswoord="";
+    
+    for (let teller=0; teller<=tekens; teller++) {
+        let random = Math.floor(Math.random() * keuze.length) +1 ;
+        let karakter = keuze[random-1];
+        paswoord+= karakter; 
+    }
+    return paswoord;
+}
+
+let aantal_tekens = parseInt(prompt("Aantal tekens?"));
+let mijn_paswoord = paswoordMaker(aantal_tekens);
+
+console.log(mijn_paswoord);
