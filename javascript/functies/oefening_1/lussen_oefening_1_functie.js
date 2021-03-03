419560b75504549c76b0b8767ee7c9aeecc5ad89
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 12:26:01 +0100

    Functie oefening 1

---
+++ javascript/functies/oefening_1/lussen_oefening_1_functie.js
@@ -0,0 +1,20 @@
+
+//Invoer
+let een_woord = prompt("Geef een woord");
+
+
+//verwerking
+function omgekeerdWoord (woord) {
+    let afteller = woord.length-1;
+    let omgekeerd  = ""
+    while (afteller >= 0) {
+        omgekeerd += woord.charAt(afteller);
+        afteller--;  
+    }
+    return omgekeerd;
+}
+
+let resultaat = omgekeerdWoord(een_woord)
+
+//uitvoer
+console.log(resultaat);
