4063d26725ebcfdec3534c58bd387d322e1fc1ab
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-03-03 13:40:24 +0100

    Functie Oefening 2

---
+++ javascript/functies/oefening_2/lussen_oefening_2_functie.js
@@ -0,0 +1,18 @@
+// invoer
+let aantal_getallen = parseInt(prompt("Hoeveel Fibonacci getallen tonen?"));
+
+
+function aantalFibonacciGetallen (getal) {
+    let lijst = [0,1];
+    let afteller = 2;
+    while (afteller < aantal_getallen) {
+    lijst.push(lijst[lijst.length-1] + lijst[lijst.length-2] );
+    afteller++;
+    }
+    return lijst.join();
+}
+
+let resultaat = aantalFibonacciGetallen(aantal_getallen);
+
+//uitvoer
+console.log(resultaat);
