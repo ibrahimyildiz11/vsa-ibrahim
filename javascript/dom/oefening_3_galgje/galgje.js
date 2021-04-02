a1cbb0060474bdc3a37fca449f975d3ada4eb95e
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-04-02 12:11:39 +0200

    niet klaar

--- javascript/dom/oefening_3_galgje/galgje.js
+++ javascript/dom/oefening_3_galgje/galgje.js
@@ -2,23 +2,50 @@ var woorden = ["appel", "peer", "ananas", "citroen", "meloen", "sinaasappel", "k
  "braambes", "bosbes", "kiwibes", "mango", "papaya", "banaan", "druif", "mandarijn", "vijg", "perzik", "nectarine", "pruim",
  "abrikoos", "limoen", "kers", "granaatappel", "lychee", "pompelmoes", "stekelbes"];
 
-var letter = "abcdefghijklmnopqrstuvwxyz";
+let randomWoord = woorden[Math.floor(Math.random() * woorden.length)];
 
-window.addEventListener("load",function(){
+let gekozenLetterLijst=[];
+
+
+function start() {
+    nieuwSpel();
+}
+
+var alfabet = "abcdefghijklmnopqrstuvwxyz".split("").map((eenletter) => 
+`<button id='` + eenletter + `'onClick=kiesLetter('` + eenletter + `')>`+ eenletter + `</button>`).join("");
+
+/* window.addEventListener("load",function(){
 nieuwSpel();
-});
+}); */
 
 function nieuwSpel(){
+    randomWoord;
     toonVraagtekens();
+
+    document.getElementById("alfabet").innerHTML = alfabet;
 };
+
+
 
 function toonVraagtekens(){
-
+    let vraag= "";
+for(let teller=1; teller <= randomWoord.length; teller++){
+    vraag+='<span>?</span>';
+}
+document.getElementById("woord").innerHTML=vraag;
 };
 
-function kiesLetter(){
+function kiesLetter(gekozenLetter){
+    document.getElementById(gekozenLetter).setAttribute("disabled",true);
+    if(randomWoord.indexOf(gekozenLetter)>-1){
+        alert("ja, er is");
+    }
+
+    gekozenLetterLijst.push(gekozenLetter);
+    let tonengekozenLetter = gekozenLetterLijst.join("-");
+    console.log(tonengekozenLetter);
     spelGewonnen();
-    spelGewonnen();
+    spelVerloren();
 };
 
 function spelGewonnen(){
