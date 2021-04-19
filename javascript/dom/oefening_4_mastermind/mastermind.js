8b62e1eb6dcc70c39e31447472d567abd15e780f
dataweb <bert@data-web.be>
2021-04-19 10:26:29 +0200

    Feedback mastermind

--- javascript/dom/oefening_4_mastermind/mastermind.js
+++ javascript/dom/oefening_4_mastermind/mastermind.js
@@ -1,6 +1,7 @@
 
 let pogingen = 0;
 let randomGetallen = [];
+/* feedback: moet deze variabele globaal gedefineerd worden? zie regel 17 en 35 */
 let ingevoerdeGetallen = [];
 
 window.addEventListener('load', function(){    
@@ -12,11 +13,15 @@ function spelOpnieuw(){
         let numbers = Math.floor(Math.random() * 10);
         randomGetallen.push(numbers)};
     console.log(randomGetallen);
+    // feedback: dit is "input" voor de functie controleerPoging?
     ingevoerdeGetallen = document.getElementsByClassName("ingevoerd_getal");
     speelVeld();
 };
 
+/* feedback: speelVeld = nieuwePoging? */
+/* feedback: functie naam en resultaat naam zijn best gelijkaardig: speelVeld =/= nieuwe_poging */
 function speelVeld(){
+    /* feedback: geef een input ook een id dan kan je die gebruiken om de input disablen bij maakOnbruikbaar */
     let invoer = '<input class="ingevoerd_getal" type="text"></input>';
     let resultaat = "";
     for (let teller = 0; teller < 4; teller++) {
@@ -27,6 +32,7 @@ function speelVeld(){
 }
 
 function controleerPoging(){
+    /* feedback: zie regel 17 */
     let getallen = [];
     for(let teller = 0; teller < 4; teller++) {
         getallen.push(ingevoerdeGetallen[teller].value);
@@ -60,6 +66,8 @@ function controleerVierGetallen(getallen){
     }
     document.getElementById("score").innerHTML += '<br>';
 };
+
+/* feedback: een functie is best een actie: bv. toonVorigePoging */
 function vorigePoging(getallen) {
     let divVanVorigePoging = '<div class="vorige_poging">';
     for (let teller = 0; teller < 4; teller++) {
@@ -74,6 +82,7 @@ function toonBolletje(kleur){
     document.getElementById("score").innerHTML += bolletje;
 };
 
+
 function gewonnen(){
     document.getElementById("boodschap").innerHTML= "Proficiat, u bent gewonnen.";
     maakOnbruikbaar();
@@ -82,6 +91,8 @@ function verloren(){
     document.getElementById("boodschap").innerHTML= "Spijtig, u bent verloren. Correct getallen is: " + randomGetallen;
     maakOnbruikbaar();
 };
+
+/* feedback: maak wat onbruikbaar? beter maakInvoerInbruikbaar */
 function maakOnbruikbaar(){
     document.getElementById("controlleer").disabled = true;
 };
