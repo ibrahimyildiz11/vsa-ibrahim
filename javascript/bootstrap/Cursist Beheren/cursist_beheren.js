b463814b1735ad092ef46b3675b0f88a83dd84b3
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2021-05-28 12:36:52 +0200

    no message

--- javascript/bootstrap/Cursist Beheren/cursist_beheren.js
+++ javascript/bootstrap/Cursist Beheren/cursist_beheren.js
@@ -10,95 +10,6 @@ window.addEventListener('load', function(){
      }
 });
 
-
-
-//READ
-async function dwapiRead(parameters) {   
-    let url = parameters.endpoint + 
-        "?project=" + parameters.project + 
-        "&entity=" + parameters.entity;
-    
-    if (typeof parameters.filter !== "undefined") {
-        url = url + "&filter=" + encodeURIComponent(JSON.stringify(parameters.filter))
-    }
-
-    let response = await fetch(url, {
-        method: 'GET'
-    });
-
-    return response.json()
-        .then(data => {             
-            return data; 
-        });
-} 
-
-// CREATE
-async function dwapiCreate(parameters) {   
-    let url = parameters.endpoint + 
-        "?project=" + parameters.project + 
-        "&entity=" + parameters.entity;
-
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token,
-        },
-        body: JSON.stringify(parameters.values)
-    });
-
-    return response.json()
-        .then(data => {             
-            return data; 
-        }); 
-}
-
-// UPDATE
-async function dwapiUpdate(parameters) {   
-    let url = parameters.endpoint + 
-        "?project=" + parameters.project + 
-        "&entity=" + parameters.entity + 
-        "&filter=" + encodeURIComponent(JSON.stringify(parameters.filter));
-
-    const response = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + parameters.token,
-        },
-        body: JSON.stringify(parameters.values)
-        
-    });
-
-    return response.json()
-        .then(data => {             
-            return data; 
-        }); 
-}
-
-
-// DELETE
-async function dwapiDelete(parameters) {   
-    let url = parameters.endpoint + 
-        "?project=" + parameters.project + 
-        "&entity=" + parameters.entity;
- 
-    const response = await fetch(url, {
-        method: 'DELETE',
-        mod: 'cors',
-        headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + parameters.token,
-        },
-        body: JSON.stringify({"filter": parameters.filter})
-    }); 
-
-    return response.json()
-        .then(data => {             
-            return data; 
-        }); 
-}
-
 //CURSISTEN TONEN
 function toonCursist() {
     let read_parameters = {
@@ -152,22 +63,19 @@ create_request.then(data => {
 };
 
 //CURSIST VERWIJDEREN
-function verwijderenCursist() {
-    let read_parameters = {
-            "endpoint": endpoint,
-            "project": project,
-            "entity": "Cursist",
-            "values": values,
-            "token": token
+function cursistVerwijderen(cursist_id) {
+    // INVOER
+    let parameters = {
+        "endpoint": endpoint, 
+        "project": project,
+        "token": token, 
+        "entity": "Cursist",
+        "filter": ["id", "=", cursist_id]}
 
-        };
-
-    let read_request = dwapiDelete(parameters);
-        request.then(data => {
-        console.log(data);
-        });
+    // VERWERKING
+    dwapiDelete(parameters).then(
+        resultaat => {
+            //verwerkResultaatNaProductActie(resultaat, "modal_product_verwijderen");
+        }
+    )
 }
-
-
-
-
